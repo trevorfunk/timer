4628dedb18275e247e00dd77717eaae8f7d9ddbe
trevorfunk <trevorjonfunk@gmail.com>
2022-06-15 11:55:33 -0700

    created timer using async programming

---
+++ timer1.js
@@ -0,0 +1,19 @@
+const input = process.argv.slice(2); //Variable for user input
+
+const timer = function() {
+ for (const number of numbers) { //Loop through every input from user
+  setTimeout (() => {
+  return process.stdout.write('\x07'); //makes beep sound
+  }, (number * 1000)) //mulitply by 1000 for seconds
+ }
+}
+
+timer(input);
+
+
+
+
+
+
+
+
